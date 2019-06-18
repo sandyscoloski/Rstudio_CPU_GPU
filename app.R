cadf0a5af138ce1a38c22b3b47e902b76edcb405
sandyscoloski <39198481+sandyscoloski@users.noreply.github.com>
2019-06-17 22:36:25 -0300

    Add files via upload

--- app.R
+++ app.R
@@ -1,4 +1,6 @@
 #
+# Created By Sandy Scoloski and Nicolas Lodea
+#
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
@@ -7,29 +9,20 @@
 #    http://shiny.rstudio.com/
 #
 
-# DPLRY
-
 library(shiny)
 library(ggplot2)
 library(plotly)
 library(dplyr)
 
-setwd("C:/Users/ASUS/Documents/Rstudio_CPU_GPU-master")
+
+setwd("D:/Documents/Rstudio_CPU_GPU-master")
 all_gpu <- read.csv2("ALL_GPUs.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
-all_cpu <- read.csv2("Intel_CPUs.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+# all_cpu <- read.csv2("Intel_CPUs.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 
 gpu <- all_gpu %>%
    select(Architecture, Manufacturer, Name, Boost_Clock, Core_Speed, L2_Cache, Max_Power,
           Memory, Memory_Bandwidth, Memory_Bus, Memory_Speed, Memory_Type,
           Pixel_Rate, Process, ROPs, Texture_Rate, TMUs)
-
-# withGPU <- subset(gpu, (grepl("Yes", gpu$Notebook_GPU)))
-# withGPU <- subset(gpu$Name, (grepl("Yes", gpu$Notebook_GPU)))
-# colnames(gpu)
-
-# gpu[c("Dedicated", "Direct_X", "DisplayPort_Connection", "HDMI_Connection",
-#       "Integrated", "Open_GL", "Power_Connector", "ROPs", "VGA_Connection",
-#       "Shader", "Release_Price", "DVI_Connection")] <- NULL
 
 gpu$Boost_Clock <- gsub("[- MHz]", "", gpu$Boost_Clock)
 gpu$Core_Speed <- gsub("[\n- MHz]", "", gpu$Core_Speed)
@@ -40,7 +33,10 @@ gpu$Memory_Bus <- gsub("[ Bit]", "", gpu$Memory_Bus)
 gpu$Max_Power <- gsub("[ Watts]", "", gpu$Max_Power)
 gpu$Pixel_Rate <- gsub("[ GPixel/s]", "", gpu$Pixel_Rate)
 gpu$Process <- gsub("[nm]", "", gpu$Process)
-gpu$L2_Cache <- gsub("[KB]", "", gpu$L2_Cache)
+gpu$L2_Cache <- gsub("[KB()]", "", gpu$L2_Cache)
+gpu$L2_Cache <- gsub("[x]", ".", gpu$L2_Cache)
+gpu$ROPs <- gsub("[ ()]", "", gpu$ROPs)
+gpu$ROPs <- gsub("[x]", ".", gpu$ROPs)
 gpu$Texture_Rate <- gsub("[ GTexel/s]", "", gpu$Texture_Rate)
 
 gpu$Boost_Clock[gpu$Boost_Clock == ""] <- NA
@@ -65,11 +61,12 @@ gpu$Memory_Bus <- as.numeric(gpu$Memory_Bus)
 gpu$Max_Power <- as.numeric(gpu$Max_Power)
 gpu$Pixel_Rate <- as.numeric(gpu$Pixel_Rate)
 gpu$Process <- as.numeric(gpu$Process)
-# gpu$L2_Cache <- as.numeric(gpu$L2_Cache)
+gpu$L2_Cache <- as.numeric(gpu$L2_Cache)
 gpu$Texture_Rate <- as.numeric(gpu$Texture_Rate)
-# gpu$ROPs <- as.numeric(gpu$ROPs)
+gpu$ROPs <- as.numeric(gpu$ROPs)
 
 numericos <- Filter(is.numeric, gpu)
+grandezas <- c("MHz", "MHz", "KB", "Watts", "MB", "GB/sec", "Bit", "MHz", "GPixel/s", "nm", "unit", "GTexel/s", "unit")
 
 # parcial <- gpu %>%
    # select(Name, Memory_Bus, Memory_Speed) %>%
@@ -88,7 +85,7 @@ numericos <- Filter(is.numeric, gpu)
    # layout(title = "Sandy Bridge Architecture")
 
 # grafico
-# gpu <- mutate(gpu, PSU_Watt = (gsub("")))
+
 variaveis <- c(colnames(numericos))
 variaveis <- sort(variaveis)
 
@@ -125,15 +122,6 @@ ui <- fluidPage(
    )
 )
 
-# Animals <- c("giraffes", "orangutans", "monkeys")
-# SF_Zoo <- c(20, 14, 23)
-# LA_Zoo <- c(12, 18, 29)
-# data <- data.frame(Animals, SF_Zoo, LA_Zoo)
-
-# p <- plot_ly(gpu, x = ~Architecture, y = ~Max_Power, type = 'bar', name = 'Max Power') %>%
-  # add_trace(y = ~Texture_Rate, name = 'Txt Rate') %>%
-  # layout(yaxis = list(title = 'Count'), barmode = 'group')
-
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
@@ -141,8 +129,8 @@ server <- function(input, output) {
      
      x1 <- gpu %>% select(c(input$parameter1, input$parameter2, Name)) %>% filter(gpu$Architecture == input$arch1)
      
-     plot_ly(x1, x = ~Name, y = x1[, input$parameter1], name = input$parameter1, type = 'bar', orientation = 'v') %>%
-       add_trace(y = x1[, input$parameter2], name = input$parameter2) %>%
+     plot_ly(x1, x = ~Name, y = x1[, input$parameter1], name = paste(input$parameter1, "(", grandezas[which(colnames(numericos) == input$parameter1)], ")"), type = 'bar', orientation = 'v') %>%
+       add_trace(y = x1[, input$parameter2], name = paste(input$parameter2, "(", grandezas[which(colnames(numericos) == input$parameter2)], ")")) %>%
        layout(title = paste(input$arch1, "Comparison", input$parameter1, " X ", input$parameter2))
 
    })
